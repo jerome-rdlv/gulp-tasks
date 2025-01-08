14ecbf148734c3fbb1f3987d7ff46a2ed0b5c3ae
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2025-01-08 21:35:51 +0100

    Remove autoprefixer, it is auto run by postcss-preset-env

--- defaults/scss.js
+++ defaults/scss.js
@@ -1,7 +1,6 @@
 module.exports = {
     engine: 'dart',
     transforms: [
-        require('autoprefixer'),
         require('postcss-pxtorem')(require('../defaults/pxtorem')),
         require('postcss-preset-env'),
     ],
