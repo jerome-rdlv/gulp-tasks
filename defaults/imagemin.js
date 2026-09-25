8d513b983987212a50c20e360bd4138f773e9ed6
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2026-09-25 13:12:20 +0200

    Disable broken mozjpeg

--- defaults/imagemin.js
+++ defaults/imagemin.js
@@ -2,7 +2,8 @@ const imagemin = require('gulp-imagemin');
 
 module.exports = [
 	imagemin.gifsicle({interlaced: true}),
-	imagemin.mozjpeg({progressive: true, quality: 85}),
+	// broken
+	// imagemin.mozjpeg({progressive: true, quality: 85}),
 	// levels greater than 0 causes some black PNGs on Safari
 	imagemin.optipng({optimizationLevel: 0})
 ];
